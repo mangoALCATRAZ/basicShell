0e49db271ef1a57833da78292ad55bc6d8f6945c
matt <angeluccimatt4@gmail.com>
2019-02-18 12:44:33 -0500

    began work on terminal prompt and keyboard input settings for interface mode

--- basic shell/main.c
+++ basic shell/main.c
@@ -1,8 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main()
 {
-    printf("Hello world!\n");
+    int batch_mode = 0;
+
+    char cmdRaw[4];
+    char *cmd;
+    char curDirectory[50];
+
+    if(batch_mode == 0){
+        printf("%s", "myshell");
+
+        if(getcwd(curDirectory, sizeof(curDirectory)) != NULL){
+            printf("%s%s%s", ": ", curDirectory, "> ");
+        }
+        else{
+            printf("%s", "> ");
+        }
+
+        if(fgets(cmdRaw, sizeof(cmdRaw), stdin)){
+
+        }
+        else{
+            printf("\n&s\n\n", "Input error!");
+            exit(1);
+        }
+    }
+
+    else{
+        exit(1);
+    }
+
+    if(strcmp(cmdRaw, "quit") == 0){ // broken. new string needs to be allocated and individual words need to be tokenized
+        printf("\n\n%s\n", "Exiting...");
+    }
     return 0;
 }
