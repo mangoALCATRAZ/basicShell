81fb31f31be0fab87e6366459e23dc911e0b97ed
matt <angeluccimatt4@gmail.com>
2019-02-21 12:40:40 -0500

    added main loop, tokenized input command string

--- basic shell/main.c
+++ basic shell/main.c
@@ -9,55 +9,67 @@ int main()
 {
     int batch_mode = 0;
 
+    int exitFlag = 0;
+
     char cmdRaw[MAX];
     char *cmd;
     char * out;
     char curDirectory[50];
 
-    if(batch_mode == 0){
-        printf("%s", "myshell");
+    while(exitFlag == 0){
+        if(batch_mode == 0){
+            printf("%s", "myshell");
 
-        if(getcwd(curDirectory, sizeof(curDirectory)) != NULL){
-            printf("%s%s%s", ": ", curDirectory, "> ");
+            if(getcwd(curDirectory, sizeof(curDirectory)) != NULL){
+                printf("%s%s%s", ": ", curDirectory, "> ");
+            }
+            else{
+                printf("%s", "> ");
+            }
+
+            if(fgets(cmdRaw, sizeof(cmdRaw), stdin)){
+
+            }
+            else{
+                printf("\n%s\n\n", "Input error!");
+                exit(1);
+            }
+
+            int incrementor = 0;
+
+            while(cmdRaw[incrementor] != '\n' && cmdRaw[incrementor] != '\0'){
+
+
+                incrementor++;
+            }
+
+            if(cmdRaw[incrementor] == '\n'){
+                cmdRaw[incrementor] = '\0';
+            }
+
+            out = (char*) malloc(incrementor * sizeof(char));
+
+            strcpy(out, cmdRaw);
+            printf("ok");
+
         }
+
         else{
-            printf("%s", "> ");
-        }
-
-        if(fgets(cmdRaw, sizeof(cmdRaw), stdin)){
-
-        }
-        else{
-            printf("\n%s\n\n", "Input error!");
             exit(1);
         }
 
-        int incrementor = 0;
+        char * token = strtok(out, " ");
 
-        while(cmdRaw[incrementor] != '\n' && cmdRaw[incrementor] != '\0'){
-
-
-            incrementor++;
+        if(strcmp(token, "quit") == 0){ // broken. new string needs to be allocated and individual words need to be tokenizedt
+            printf("\n\n%s\n", "Exiting...");
+            exit(0);
+        }
+        else if(strcmp(token, "cd") == 0){
+            printf("\n\n%s\n", "You selected cd");
         }
 
-        if(cmdRaw[incrementor] == '\n'){
-            cmdRaw[incrementor] = '\0';
-        }
-
-        out = (char*) malloc(incrementor * sizeof(char));
-
-        strcpy(out, cmdRaw);
-        printf("ok");
-
     }
 
-    else{
-        exit(1);
-    }
-
-    if(strcmp(out, "quit") == 0){ // broken. new string needs to be allocated and individual words need to be tokenizedt
-        printf("\n\n%s\n", "Exiting...");
-    }
     return 0;
 }
 
