f5302e3864a8972dba4ce5e98a57bce1e0310658
matt <angeluccimatt4@gmail.com>
2019-02-21 00:15:15 -0500

    Added input text recognition and formatting functionality. Prints current directory in prompt

--- basic shell/main.c
+++ basic shell/main.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+
+#define MAX 200
 
 int main()
 {
     int batch_mode = 0;
 
-    char cmdRaw[4];
+    char cmdRaw[MAX];
     char *cmd;
+    char * out;
     char curDirectory[50];
 
     if(batch_mode == 0){
@@ -24,17 +28,36 @@ int main()
 
         }
         else{
-            printf("\n&s\n\n", "Input error!");
+            printf("\n%s\n\n", "Input error!");
             exit(1);
         }
+
+        int incrementor = 0;
+
+        while(cmdRaw[incrementor] != '\n' && cmdRaw[incrementor] != '\0'){
+
+
+            incrementor++;
+        }
+
+        if(cmdRaw[incrementor] == '\n'){
+            cmdRaw[incrementor] = '\0';
+        }
+
+        out = (char*) malloc(incrementor * sizeof(char));
+
+        strcpy(out, cmdRaw);
+        printf("ok");
+
     }
 
     else{
         exit(1);
     }
 
-    if(strcmp(cmdRaw, "quit") == 0){ // broken. new string needs to be allocated and individual words need to be tokenizedt
+    if(strcmp(out, "quit") == 0){ // broken. new string needs to be allocated and individual words need to be tokenizedt
         printf("\n\n%s\n", "Exiting...");
     }
     return 0;
 }
+
