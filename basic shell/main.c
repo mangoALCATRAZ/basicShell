843adf6fb40c254174867cbf2de40a44ff29a4ec
matt <angeluccimatt4@gmail.com>
2019-03-08 21:49:07 -0500

    Added Input/Output redirection. Works as far as i've tested it so far. Probably needs more extensive testing.

--- basic shell/main.c
+++ basic shell/main.c
@@ -3,12 +3,17 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <sys/wait.h>
 
 #define MAX 200
 
-int forkLogic(char* args[]);
+int prepareOutputRedirection(char **token, char **filename);
+int prepareInputRedirection(char **token, char **filename);
+int forkLogic(char* args[], int inRedirect, int outRedirect);
 int pipeLogic(char* args[], char* pipeArgs[]);
+
 
 
 int main()
@@ -16,6 +21,12 @@ int main()
     int batch_mode = 0;
 
     int exitFlag = 0;
+
+    int inRedirect;
+    int outRedirect;
+    char * inRedirectFilename;
+    char * outRedirectFilename;
+
 
     char cmdRaw[MAX];
     char *cmd;
@@ -95,10 +106,15 @@ int main()
             char *args[64];
             char **next = args;
 
+
+
             char *pipeArgs[64];
             char **nextPipe = pipeArgs;
 
             int pipeFlag = 0;
+            int inputRedir = 0;
+            int outputTruncate = 0;
+            int outputAppend = 0;
             int errorFlag = 0;
 
             *next++ = token;
@@ -107,6 +123,18 @@ int main()
             while(token != NULL){
                 if(strcmp(token, "|") == 0){
                     pipeFlag = 1;
+                    break;
+                }
+                else if(strcmp(token, "<") == 0){
+                    inputRedir = 1;
+                    break;
+                }
+                else if(strcmp(token, ">") == 0){
+                    outputTruncate = 1;
+                    break;
+                }
+                else if(strcmp(token, ">>") == 0){
+                    outputAppend = 1;
                     break;
                 }
                 *next++ = token;
@@ -132,12 +160,120 @@ int main()
                 }
             }
 
+            else if(inputRedir == 1 && errorFlag == 0){
+                char ** tokenP = &token;
+                char ** inP = &inRedirectFilename;
+
+                int prepareInputResult = prepareInputRedirection(tokenP, inP);
+                if(prepareInputResult < 0){
+                    errorFlag = 1;
+                }
+                else if(prepareInputResult == 1){
+                    outputTruncate = 1;
+                }
+                else if(prepareInputResult == 2){
+                    outputAppend = 1;
+                }
+            }
+            else if(outputTruncate == 1 && errorFlag == 0){
+                char ** tokP = &token;
+                char ** outP = &outRedirectFilename;
+                int prepareOutputTruncResult = prepareOutputRedirection(tokP, outP);
+                if(prepareOutputTruncResult < 0){
+                    errorFlag = 1; // output redirection filename obtaining failed
+                }
+                else if(prepareOutputTruncResult > 0){
+                    if(inputRedir == 1){ // cannot have multiple input redirections
+                        errorFlag = 1;
+                    }
+                    else{
+                        char ** inP2 = &inRedirectFilename;
+                        int inAfterOutResult = prepareInputRedirection(tokP, inP2);
+                        if(inAfterOutResult < 0){ // input redirection filename obtaining failed
+                            errorFlag = 1;
+                        }
+                        else if(inAfterOutResult > 0){ // cannot have multiple input or output redirections
+                            errorFlag = 1;
+                        }
+                        else{
+                            inputRedir = 1;
+                        }
+                    }
+                }
+            }
+            else if(outputAppend == 1 && errorFlag == 0){
+                char ** tokP = &token;
+                char ** outP = &outRedirectFilename;
+                int prepareOutputAppendResult = prepareOutputRedirection(tokP, outP);
+                if(prepareOutputAppendResult < 0){
+                    errorFlag = 1;
+                }
+                else if(prepareOutputAppendResult > 0){
+                    if(inputRedir == 1){
+                        errorFlag = 1; // cannot have multiple input redirections
+                    }
+                    else{
+                        char ** inP2 = &inRedirectFilename;
+                        int inAfterAppendOutResult = prepareInputRedirection(tokP, inP2);
+                        if(inAfterAppendOutResult < 0){
+                            errorFlag = 1;
+                        }
+                        else if(inAfterAppendOutResult > 0){
+                            errorFlag = 1;
+                        }
+                        else{
+                            inputRedir = 1;
+                        }
+                    }
+                }
+            }
+
             if(errorFlag == 0){ // if no errors, proceed
                 if(pipeFlag == 1){
                     pipeLogic(args, pipeArgs);
                 }
                 else{
-                    forkLogic(args);
+                    if(inputRedir == 1){
+                        inRedirect = open(inRedirectFilename, O_RDWR | O_CREAT, S_IRWXU | S_IRWXO);
+                        if(inRedirect == -1){
+                            errorFlag = 1;
+                        }
+                    }
+                    if(outputAppend == 1 && errorFlag == 0){
+                        outRedirect = open(outRedirectFilename, O_RDWR | O_CREAT | O_APPEND, S_IRWXU | S_IRWXO);
+                        if(outRedirect == -1){
+                            errorFlag = 2;
+                        }
+                    }
+                    else if(outputTruncate == 1 && errorFlag == 0){
+                        outRedirect = open(outRedirectFilename, O_RDWR | O_CREAT | O_TRUNC, S_IRWXU | S_IRWXO);
+                        if(outRedirect == -1){
+                            errorFlag = 2;
+                        }
+                    }
+
+                    if(errorFlag == 0){
+                        if(inputRedir == 1 && outputTruncate == 0 && outputAppend == 0){
+                            forkLogic(args, inRedirect, -1);
+                        }
+                        else if(inputRedir == 1 && (outputTruncate == 1 || outputAppend == 1)){
+                            forkLogic(args, inRedirect, outRedirect);
+                        }
+                        else if(inputRedir == 0 && outputTruncate == 0 && outputAppend == 0){
+                            forkLogic(args, -1, -1);
+                        }
+                        else{
+                            forkLogic(args, -1, outRedirect);
+                        }
+                    }
+                    else{
+                        if(errorFlag == 1){
+                            printf("\n\n%s\n", "Error in opening or creating input redirection file for this command.");
+                        }
+                        else if(errorFlag == 2){
+                            printf("\n\n%s\n", "Error in opening or creating output redirection file for this command");
+                        }
+                    }
                 }
             }
             else{
@@ -152,9 +288,62 @@ int main()
     return 0;
 }
 
-int forkLogic(char* args[]){
+int prepareOutputRedirection(char **token, char **filename){
+    int ret = 0;
+
+    *token = strtok(NULL, " ");
+
+    if(*token == NULL){ // may need to add conditions for additional special character input in lieu of acceptable filename. watch out for this in testing
+        ret = -1; //error
+    }
+    else{
+        *filename = *token;
+        *token = strtok(NULL, " ");
+        if(*token != NULL){
+            if(strcmp(*token, "<") != 0){
+                ret = -1; //error
+            }
+            else if(strcmp(*token, "<") == 0){
+                ret = 1; // additional input redirection detected
+            }
+        }
+    }
+
+    return ret;
+}
+
+int prepareInputRedirection(char **token, char **filename){
+    int ret = 0;
+
+    *token = strtok(NULL, " ");
+
+    if(*token == NULL){ // see equivalent comment in prepareOutputRedirection()
+        ret = -1; // error
+    }
+    else{
+        *filename = *token;
+        *token = strtok(NULL, " ");
+        if(*token != NULL){
+            if(strcmp(*token, ">") != 0 && strcmp(*token, ">>") != 0){
+                ret = -1; // error
+            }
+            else if(strcmp(*token, ">") == 0){
+                ret = 1; // additional output redirection detected.
+            }
+            else if(strcmp(*token, ">>") == 0){
+                ret = 2; // additional output append redirection detected
+            }
+        }
+    }
+    return ret;
+}
+
+int forkLogic(char* args[], int inRedirect, int outRedirect){
     int pid;
     int ret = 0;
+
+    int inFlag = 0;
+    int outFlag = 0;
 
     pid = fork();
 
@@ -163,6 +352,15 @@ int forkLogic(char* args[]){
         ret = 1;
     }
     else if(pid == 0){ // CHILD PROCESS
+        if(inRedirect != -1){
+            int check1 = dup2(inRedirect, 0);
+            inFlag = 1;
+        }
+        if(outRedirect != -1){
+            int check2 = dup2(outRedirect, 1);
+            outFlag = 1;
+        }
+
         int execute = execvp(args[0], args);
         if(execute != 0){
             printf("\n\nExecution Error in fork");
@@ -171,6 +369,12 @@ int forkLogic(char* args[]){
     }
     else{ // PARENT PROCESS
         wait(NULL);
+        if(inFlag == 1){
+            close(inRedirect);
+        }
+        if(outFlag == 1){
+            close(outRedirect);
+        }
     }
 
     return ret;
